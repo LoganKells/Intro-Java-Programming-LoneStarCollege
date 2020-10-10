a1850f99ebaff29e091c13564c70301b8f03db04
technetium-43 <58450924+technetium-43@users.noreply.github.com>
2020-10-10 18:25:14 -0500

    Chapter 1 Exercises

---
+++ chapter1exercises/src/com/exercises/Exercise_01_01.java
@@ -0,0 +1,12 @@
+package com.exercises;
+
+// Created by Logan Kells on 2020-08-24
+// 1.1 (Display three messages)
+// Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.
+
+public class Exercise_01_01 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!");
+        System.out.println("Learning Java Now");
+    }
+}
---
+++ chapter1exercises/src/com/exercises/Exercise_01_02.java
@@ -0,0 +1,13 @@
+// Created by Logan Kells on 2020-08-24
+// 1.2 (Display five messages) Write a program that displays I love Java five times.
+package com.exercises;
+
+public class Exercise_01_02 {
+    public static void main(String[] args){
+        System.out.print("I Love Java\n");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+    }
+}
